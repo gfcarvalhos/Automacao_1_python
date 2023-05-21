84ee6012475ebfc90d653bd7574ca028141af04f
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2023-05-20 21:32:50 -0300

    Downloading basedata from drive

--- projetoAutomacao1.py
+++ projetoAutomacao1.py
@@ -12,16 +12,24 @@ pyautogui.press("enter")
 pyautogui.write("https://pages.hashtagtreinamentos.com/aula1-intensivao-sistema")
 pyautogui.press("enter")
 
-time.sleep(3)
+time.sleep(5)
 
 # Passo 2: Fazer login no sistema
-pyautogui.click(x=1157, y=525)
+pyautogui.click(x=820, y=475)
 pyautogui.write("gfcarvalho")
-
-pyautogui.click(x=962, y=629)
+pyautogui.click(x=787, y=578)
 pyautogui.write("12345")
-
-pyautogui.click(x=1114, y=711)
+pyautogui.click(x=950, y=652)
 
 time.sleep(3)
 #print(pyautogui.position())
+
+# Passo 3: Baixar base dados
+pyautogui.click(x=486, y=533, button = "right")
+pyautogui.click(x=589, y=839)
+
+# Passo 4: Calcular indicadores
+
+
+
+#Passo 5: Enviar e-mail
