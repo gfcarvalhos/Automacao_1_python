d22a50ca545c89265ac211e804f39beae28eada5
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2023-05-20 21:19:14 -0300

    Login system

--- projetoAutomacao1.py
+++ projetoAutomacao1.py
@@ -11,3 +11,17 @@ pyautogui.write("chrome")
 pyautogui.press("enter")
 pyautogui.write("https://pages.hashtagtreinamentos.com/aula1-intensivao-sistema")
 pyautogui.press("enter")
+
+time.sleep(3)
+
+# Passo 2: Fazer login no sistema
+pyautogui.click(x=1157, y=525)
+pyautogui.write("gfcarvalho")
+
+pyautogui.click(x=962, y=629)
+pyautogui.write("12345")
+
+pyautogui.click(x=1114, y=711)
+
+time.sleep(3)
+#print(pyautogui.position())
